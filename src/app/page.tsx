ca47a1d8267a9b939d9589ac8c7f07f9ea0242b6
ilyatovbin <ilyaxiv0@gmail.com>
2025-08-13 13:00:41 -0700

    Dashboard set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 
+
+import DashboardLayout from './dashboard/layout';
 import HomeRedirect from './HomeRedirect';
+
 
 export default function Home() {
   return (
-    <>
+    <DashboardLayout>
       <HomeRedirect />
       <main>
-        Hello World!
+        
       </main>
-    </>
+    </DashboardLayout>
   );
 }
